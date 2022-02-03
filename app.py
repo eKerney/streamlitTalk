490124e20c775b8dffc0d0bf737dc08c90d4ce8c
eKerney <Eric.Kerney@airspacelink.com>
2022-02-03 12:17:33 -0500

    first prez commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import streamlit_book as stb
+
+# Streamlit webpage properties
+st.set_page_config(page_title="HappyBirds", page_icon="🐦")
+
+# Streamlit book properties
+stb.set_book_config(path="slides")
